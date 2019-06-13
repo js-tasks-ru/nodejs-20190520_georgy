945b69e415e5bf7facd138090ea83af6d95120b3
ACSAIS <warlack@outlook.com>
2019-06-13 07:23:33 +0300

    test

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -6,11 +6,19 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const messages = [];
+
 
 router.get('/subscribe', async (ctx, next) => {
+  console.log('/subscribe');
+  return await next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  console.log('/publish');
+  messages.push(ctx.request.body.message);
+  ctx.res.end(Buffer.from(messages));
+  return next();
 });
 
 app.use(router.routes());
