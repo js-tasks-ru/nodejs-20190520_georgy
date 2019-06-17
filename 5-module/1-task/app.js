3cc305faf81720a5d3472d35fec7c7ce81aef31d
ACSAIS <warlack@outlook.com>
2019-06-17 12:54:52 +0300

    6 module 1 task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,14 +8,12 @@ const Router = require('koa-router');
 const router = new Router();
 const subscribers = [];
 router.get('/subscribe', async (ctx, next) => {
-  console.log('subscribe');
   ctx.body = await new Promise((resolve) => {
     subscribers.push(resolve);
   });
 });
 
 router.post('/publish', async (ctx, next) => {
-  console.log('publish');
   const {message} = ctx.request.body;
   if (message) {
     subscribers.forEach((subscriber) => subscriber(message));
