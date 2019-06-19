fcb7461726c2a11a571ff3f68bc1e4f4fc91c51a
ACSAIS <warlack@outlook.com>
2019-06-19 23:35:19 +0300

    6 module 2 task

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,7 +1,8 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-
+const User = require('./models/User');
 const app = new Koa();
+const mongoose = require('mongoose');
 
 app.use(require('koa-static')('public'));
 app.use(require('koa-bodyparser')());
@@ -22,25 +23,80 @@ app.use(async (ctx, next) => {
 
 const router = new Router();
 
-router.get('/users', async (ctx) => {
-
+router.get('/users', async (ctx, next) => {
+  ctx.body = await User.find({});
 });
 
 router.get('/users/:id', async (ctx) => {
-
+  const {id} = ctx.params;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const user = await User.findOne({_id: id});
+    if (user) {
+      ctx.body = user;
+    } else {
+      ctx.status = 404;
+    }
+  } else {
+    ctx.status = 400;
+  }
 });
 
 router.patch('/users/:id', async (ctx) => {
-
+  const {id} = ctx.params;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const {email, displayName} = ctx.request.body;
+    try {
+      const user = await User.findOne({_id: id});
+      if (email) {
+        user.email = email;
+      }
+      if (displayName) {
+        user.displayName = displayName;
+      }
+      ctx.body = await user.save();
+    } catch (e) {
+      ctx.status = 400;
+      ctx.body = {errors: createValidationErrors(e.errors)};
+    }
+  } else {
+    ctx.status = 400;
+  }
 });
 
 router.post('/users', async (ctx) => {
-
+  const {email, displayName} = await ctx.request.body;
+  console.log(email, displayName);
+  try {
+    ctx.body = await User.create({email: email, displayName: displayName});
+  } catch ({errors}) {
+    const validationErrors = createValidationErrors(errors);
+    ctx.status = 400;
+    ctx.body = {errors: validationErrors};
+  }
 });
 
 router.delete('/users/:id', async (ctx) => {
-
+  const {id} = ctx.params;
+  try {
+    const {deletedCount} = await User.remove({_id: id});
+    ctx.status = 200;
+    if (deletedCount === 0) {
+      console.log(123);
+      ctx.status = 404;
+    } else {}
+  } catch (e) {
+    ctx.status = 404;
+  }
 });
+
+const createValidationErrors = (errors) => {
+  const validationErrors = {};
+  // eslint-disable-next-line guard-for-in
+  for (const key in errors) {
+    validationErrors[key] = errors[key].message;
+  }
+  return validationErrors;
+}
 
 app.use(router.routes());
 
